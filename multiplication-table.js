6dc5a8a92f44b737e3a546f63c9ce01bdafa998b
Alvintol <alvintol+github@hotmail.com>
2022-02-15 21:17:48 +0000

    Create a function that builds a multiplication table with a maximum value provided

---
+++ multiplication-table.js
@@ -0,0 +1,20 @@
+// Conditions
+/*
+ - Print multiplication tables to the console from 1 to given number.
+ - Number provided will be the highest value of our table.
+*/
+const multiplicationTable = function (maxValue) {
+  let table = "";
+  for (let a = 1; a <= maxValue; a++) {
+    for (let b = 1; b <= maxValue; b++) {
+      console.log(b);
+      table += a * b + " ";
+    }
+    table += "\n";
+  }
+  return table;
+};
+
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));
