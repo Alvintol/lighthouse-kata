59665cacce7dae430fba7d477c715538b865471d
Alvintol <alvintol+github@hotmail.com>
2022-02-18 18:24:08 +0000

    Create an algorithm to detect if two queens on a generated chessboard are a threat to each other

---
+++ queen-threat.js
@@ -0,0 +1,43 @@
+// Conditions
+/*
+Create an alogrithm to detect if two queens on a chess board can attack each other. 
+*/
+
+function generateBoard(whiteQueen, blackQueen) {
+  let board = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+  for (let a = 0; a < board.length; a++) {// a = rows
+    for (let b = 0; b < board[a].length; b++) {// b = columns
+      board[whiteQueen[0]][whiteQueen[1]] = 1;
+      board[blackQueen[0]][blackQueen[1]] = 1;
+    }
+  }
+  return board;
+}
+
+function queenThreat() {
+  for (let c = 0; c < whiteQueen.length; c++) {
+    for (let d = 0; d < blackQueen.length; d++) {
+      if (whiteQueen[c] === blackQueen[d]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+let whiteQueen = [0, 7];
+let blackQueen = [5, 7];
+// let whiteQueen = [0, 5]; other positions to check
+// let blackQueen = [5, 0]; other positions to check
+let generatedBoard = generateBoard(whiteQueen, blackQueen);
+console.log(generatedBoard);
+console.log(queenThreat(generatedBoard));
