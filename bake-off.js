0e79c39592182e687b172313817ab05cb12fbefc
Alvintol <alvin.tol@hotmail.com>
2022-03-11 02:43:29 +0000

    refractor

--- bake-off.js
+++ bake-off.js
@@ -1,38 +1,21 @@
-//Conditions
-/*
- - Create a function that recieves 3 parameters:
-  - An array of ingedients in stock at Bakery A
-  - An array of ingedients in stock at Bakery B
-  - An Array of recipe objects:
-   - strings: names
-   - array: ingredients
- - Find a recipe that utilizes one ingredient from both Bakeries
-*/
+//revisted code. Refractored
+const chooseRecipe = (bakeryA, bakeryB, recipes) => {
+  let recipeChoice = [];
 
-const chooseRecipe = function (bakeryA, bakeryB, recipes) {
-  let choice = "";
-  for (let a = 0; a < bakeryA.length; a++) {
-    // console.log(bakeryA[a]);
-    for (let b = 0; b < bakeryB.length; b++) {
-      // console.log(bakeryB[b]);
-      for (let c = 0; c < recipes.length; c++) {
-        // console.log(recipes[c].ingredients[0], recipes[c].ingredients[1])
-        if (
-          bakeryA[a] === recipes[c].ingredients[0] ||
-          bakeryA[a] === recipes[c].ingredients[1]
-        ) {
-          if (
-            bakeryB[b] === recipes[c].ingredients[0] ||
-            bakeryB[b] === recipes[c].ingredients[1]
-          ) {
-            choice = recipes[c].name;
-          }
+  bakeryA.forEach(itemA => {
+    bakeryB.forEach(itemB => {
+      for (let recipe in recipes) {
+        if (itemA === recipes[recipe].ingredients[0] && itemB === recipes[recipe].ingredients[1] ||
+          itemA === recipes[recipe].ingredients[1] && itemB === recipes[recipe].ingredients[0]) {
+          recipeChoice.push(recipes[recipe].name);
         }
       }
-    }
-  }
-  return choice;
+    })
+  })
+  return recipeChoice;
 };
+
+
 
 let bakeryA = ["saffron", "eggs", "tomato paste", "coconut", "custard"];
 let bakeryB = ["milk", "butter", "cream cheese"];
@@ -71,3 +54,27 @@ recipes = [
 ];
 
 console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+
+// ********* original code ***
+
+const chooseRecipe = function (bakeryA, bakeryB, recipes) {
+  let choice = "";
+  for (let a = 0; a < bakeryA.length; a++) {
+    for (let b = 0; b < bakeryB.length; b++) {
+      for (let c = 0; c < recipes.length; c++) {
+        if (
+          bakeryA[a] === recipes[c].ingredients[0] ||
+          bakeryA[a] === recipes[c].ingredients[1]
+        ) {
+          if (
+            bakeryB[b] === recipes[c].ingredients[0] ||
+            bakeryB[b] === recipes[c].ingredients[1]
+          ) {
+            choice = recipes[c].name;
+          }
+        }
+      }
+    }
+  }
+  return choice;
+};
